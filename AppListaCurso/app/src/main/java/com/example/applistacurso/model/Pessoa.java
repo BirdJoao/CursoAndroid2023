06fe226866aaeae6dc8c1f4be3dd2eb986fcdbde
joao <rze12781@gmail.com>
2023-12-18 13:19:15 -0300

    Criando classe Pessoa

---
+++ AppListaCurso/app/src/main/java/com/example/applistacurso/model/Pessoa.java
@@ -0,0 +1,53 @@
+package com.example.applistacurso.model;
+
+public class Pessoa {
+
+    private String nome;
+    private String sobrenome;
+    private String curso;
+    private String telefone;
+    public Pessoa() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", sobrenome='" + sobrenome + '\'' +
+                ", curso='" + curso + '\'' +
+                ", telefone='" + telefone + '\'' +
+                '}';
+    }
+}
